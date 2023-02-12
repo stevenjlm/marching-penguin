096dc5f71c5aa7bd7e21a3f9582b1f324e7aa145
Steven <stevenjmunn@gmail.com>
2023-02-11 17:01:26 -0800

    24 hour model deployed

--- src/data/synthesizer.py
+++ src/data/synthesizer.py
@@ -42,12 +42,57 @@ class OneHotColumn(SignalSynthesizerMethod):
         return raw_signal
 
 
+class ConvolveBoleanColumns(SignalSynthesizerMethod):
+    DEFAULT_DELTA_T = 3
+
+    def __init__(self, cols: List[str], in_place: List[bool], delta_ts: List[int] = None) -> None:
+        super().__init__()
+        self.col_names = cols
+        self.in_place = in_place
+        self.delta_ts = delta_ts
+
+    def generate(self, raw_signal: pd.DataFrame, events: pd.DataFrame) -> pd.DataFrame:
+        """
+        Convolutions need to happen per machine, one at a time.
+        """
+        machine_ids = raw_signal[MACHINE_ID].unique()
+        output_signal = pd.DataFrame()
+        for machine_id in machine_ids:
+            machine_signal = raw_signal.loc[raw_signal[MACHINE_ID] == machine_id]
+            machine_signal = self.generate_machine(machine_signal)
+            output_signal = pd.concat([output_signal, machine_signal])
+        return output_signal
+
+
+    def generate_machine(self, raw_signal: pd.DataFrame) -> pd.DataFrame:
+        for idx_col in range(len(self.col_names)):
+            col_name = self.col_names[idx_col]
+            in_place = self.in_place[idx_col]
+            if self.delta_ts:
+                del_t = self.delta_ts[idx_col]
+            else:
+                del_t = self.DEFAULT_DELTA_T
+
+            numpy_column_values = raw_signal[col_name].to_numpy()
+            convolution_filter = np.ones(2 * del_t)
+            convolution_filter[del_t:] = 0
+            column_result = np.convolve(numpy_column_values, convolution_filter, 'same')
+
+            if in_place:
+                new = {col_name: column_result}
+                raw_signal = raw_signal.assign(**new)
+            else:
+                new = {col_name + "_convolve_" + str(del_t): column_result}
+                raw_signal = raw_signal.assign(**new)
+        return raw_signal
+
+
 class MachineSignalSynth:
     
     def __init__(self,
                  tel: pd.DataFrame,
                  events: pd.DataFrame,
-                 methods: List[Union[JoinColumns, OneHotColumn]]):
+                 methods: List[Union[JoinColumns, OneHotColumn, ConvolveBoleanColumns]]):
         self.methods = methods
         self.tel = tel
         self.events = events
